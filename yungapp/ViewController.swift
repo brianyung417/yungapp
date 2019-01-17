8d9625a661106b460660299fa05a353ccc38c89c
Yung <tiffanyyung@tiffanydemacbook-air.local>
2019-01-17 22:59:29 +0800

    Initial Commit

---
+++ yungapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yungapp
+//
+//  Created by Tiffany Yung on 17/1/2019.
+//  Copyright © 2019年 Tiffany Yung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
